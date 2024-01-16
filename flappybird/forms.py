88ed810d5c56288eb79d5a9f8b34c90a840fe73d
abhaykoka <135577877+abhaykoka@users.noreply.github.com>
2024-01-16 22:16:11 +0530

    Delete flappybird directory

--- flappybird/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-class DownloadForm(forms.Form):
-    name=forms.CharField(max_length=20,required=True)
-    emailaddress = forms.CharField(max_length=100, required=True)
-    num2 = forms.IntegerField(required=True)
